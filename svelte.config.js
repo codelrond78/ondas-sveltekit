c8303c70e3c6e8dae15b99e5d54d9a2abcb2142f
Lord Codelrond <codelrond@protonmail.com>
2022-03-19 18:46:28 +0100

    added charts

--- svelte.config.js
+++ svelte.config.js
@@ -3,11 +3,21 @@ import adapter from '@sveltejs/adapter-static'
 import sveltePreprocess from 'svelte-preprocess'
 import { mdsvex } from 'mdsvex'
 
+const production = process.env.NODE_ENV === 'production';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md', '.svelte.md'],
 	kit: {
 		adapter: adapter(),
+		vite: {
+			optimizeDeps: {
+				include: ['@carbon/charts'],
+			},
+			ssr: {
+				noExternal: [production && '@carbon/charts'].filter(Boolean),
+			},
+		},
 		prerender: {
 			// This can be false if you're using a fallback (i.e. SPA mode)
 			default: true
