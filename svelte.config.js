a64bc0c8cfd474c5c276ed8946dcd876a6b6aa02
Lord Codelrond <codelrond@protonmail.com>
2022-03-20 10:21:38 +0100

    vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-//import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-vercel'
+//import adapter from '@sveltejs/adapter-static'
 import sveltePreprocess from 'svelte-preprocess'
 import { mdsvex } from 'mdsvex'
 
