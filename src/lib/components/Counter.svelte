dd2715c3b6de30f9b62421ed7ba6b7a834b1863b
Lord Codelrond <codelrond@protonmail.com>
2022-03-20 10:16:35 +0100

    with counter

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { useInterval } from '@svelte-use/core'
+
+  const { counter, pause, resume } = useInterval(200, { controls: true })
+</script>
+
+<div>{$counter}</div>
+<button on:click={pause}>Pause</button>
+<button on:click={resume}>Resume</button>
