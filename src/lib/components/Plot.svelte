a18e6c524f2c5cbc9491c56937a24cc40e0ad5f3
codelrond <codelrond@protonmail.com>
2022-04-05 20:26:34 +0200

    onda function implemented, pending tests

--- src/lib/components/Plot.svelte
+++ src/lib/components/Plot.svelte
@@ -1,102 +1,43 @@
 <script>
+	import RangeSlider from "svelte-range-slider-pips";
 	import "@carbon/charts/styles.min.css";
 	import { StackedAreaChart } from "@carbon/charts-svelte";
-  import { onDestroy } from 'svelte';
+  	import { onDestroy } from 'svelte';
+	import Ring from 'ringjs';
+	var ring = new Ring(50);
 
-let counter = 0;
+	let bound = [75];
+	let counter = 0;
+	let data = [];
+	
+	function onda(t){
+		return Math.sin(2*Math.PI*t)
+	}
+
+	const t0 = 5;
+	[...Array(t0*10).keys()].forEach((i) => ring.push(i*0.1));
+
+	data = ring.toArray().map(t => ({
+		"group": "Dataset 0",
+		"date": t,
+		"value": onda(t)
+	}))	
   
-	const handle = () => {
-    data[0].value -= 1000;
-    data[1].value -= 1000;
-    data[2].value -= 1000;
-    data[3].value -= 1000;
-    data[4].value -= 1000;
-    data = data;
-    counter += 1;
-    console.log(counter)
-  }
-	const interval = setInterval(handle, 50);
+	let t = 0;
+
+	const handle = () => {				
+		ring.push(t)
+		data = ring.toArray().map(t => ({
+			"group": "Dataset 0",
+			"date": t0 + t,
+			"value": onda(t0 + t)
+		}));
+		t = t + 0.1;
+	}
+	const interval = setInterval(handle, 100);
 
 	onDestroy(() => clearInterval(interval));
 
-let data = [
-  /*{
-		"group": "Dataset 1",
-		"date": "2018-12-31T23:00:00.000Z",
-		"value": 10000
-	},
-	{
-		"group": "Dataset 1",
-		"date": "2019-01-04T23:00:00.000Z",
-		"value": 65000
-	},
-	{
-		"group": "Dataset 1",
-		"date": "2019-01-07T23:00:00.000Z",
-		"value": 10000
-	},
-	{
-		"group": "Dataset 1",
-		"date": "2019-01-12T23:00:00.000Z",
-		"value": 49213
-	},
-	{
-		"group": "Dataset 1",
-		"date": "2019-01-16T23:00:00.000Z",
-		"value": 51213
-	},*/
-	{
-		"group": "Dataset 2",
-		"date": "2018-12-31T23:00:00.000Z",
-		"value": 20000
-	},
-	{
-		"group": "Dataset 2",
-		"date": "2019-01-04T23:00:00.000Z",
-		"value": 25000
-	},
-	{
-		"group": "Dataset 2",
-		"date": "2019-01-07T23:00:00.000Z",
-		"value": 60000
-	},
-	{
-		"group": "Dataset 2",
-		"date": "2019-01-12T23:00:00.000Z",
-		"value": 30213
-	},
-	{
-		"group": "Dataset 2",
-		"date": "2019-01-16T23:00:00.000Z",
-		"value": 55213
-	},/*
-	{
-		"group": "Dataset 3",
-		"date": "2018-12-31T23:00:00.000Z",
-		"value": 30000
-	},
-	{
-		"group": "Dataset 3",
-		"date": "2019-01-04T23:00:00.000Z",
-		"value": 20000
-	},
-	{
-		"group": "Dataset 3",
-		"date": "2019-01-07T23:00:00.000Z",
-		"value": 40000
-	},
-	{
-		"group": "Dataset 3",
-		"date": "2019-01-12T23:00:00.000Z",
-		"value": 60213
-	},
-	{
-		"group": "Dataset 3",
-		"date": "2019-01-16T23:00:00.000Z",
-		"value": 25213
-	}*/
-];
-  
 </script>
 
 <StackedAreaChart
@@ -119,3 +60,5 @@ let data = [
 	"height": "400px"
 }}
 	/>
+
+<RangeSlider min={0} max={100} bind:values={bound} />
